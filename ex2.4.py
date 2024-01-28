69ce085ee10d9dcb76416f642f6a0f9e568ef451
Trevor Nguyen <100506401+lordtrev4@users.noreply.github.com>
2024-01-28 00:04:49 -0700

    Add files via upload

---
+++ ex2.4.py
@@ -0,0 +1,32 @@
+import timeit
+
+def code_measured(file):
+    with open("C:/University/VSCode/ENSF338/Lab01/pg2701.txt", "r", encoding="utf-8") as textfile:
+        textfile.seek(915) # starts from chapter 1
+        vowel_list = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'] 
+        vowel_amt = 0
+        word_amt = 0
+        textarray = textfile.readlines()
+
+        for lines in textarray:
+
+            linelist = list(lines)
+            for char in linelist:
+                if char in vowel_list:
+                    vowel_amt += 1
+
+            word = lines.split()
+
+            for i in word:
+                word_amt += 1
+
+
+    avg_vowels = (vowel_amt / word_amt)
+
+file = "C:/University/VSCode/ENSF338/Lab01/pg2701.txt"
+
+time_taken = timeit.timeit(lambda: code_measured("C:/University/VSCode/ENSF338/Lab01/pg2701.txt"), number=100)
+
+avg_time = time_taken / 100
+
+print(f'Time taken on average for 100 repititions: {avg_time} seconds.')
