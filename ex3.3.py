dbe27d2eee495fc0ab612599bdb7552c4d09e19f
Zachary Lam <zachary.lam@ucalgary.ca>
2024-01-29 14:54:01 -0700

    Add 3.3

--- ex3.3.py
+++ ex3.3.py
@@ -10,32 +10,30 @@ def change_size(n):
     i = 0
     while i < n:
         for record in data:
-                if 'size' in record['payload']:
-                    record['payload']['size'] = 42
-                    i += 1
+            if 'size' in record['payload']:
+                record['payload']['size'] = 42
+                i += 1
     return 0
 
 def change():
-    listlengths = 1000
-    time1000 = []
-    time = timeit.repeat(lambda: change_size(listlengths), number=1000)
-    time1000.append(time)
+    listlengths = [1000]
+    for listlength in listlengths:
+        rez = timeit.repeat(lambda: change_size(listlength), repeat=1000, number=1)
+    return rez
 
-    return time1000
+def histogram(rez):
+    plt.hist(rez, bins=1000)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of processing times')
+    plt.savefig('output.3.3.png')
 
 def main():
     data.reverse()
     with open('output.2.3.json', 'w', encoding='utf-8') as f:
         json.dump(data, f)
-
-    time1000 = change()
-    plt.hist(time1000, bins=100, edgecolor='black')
-    plt.title('Distribution of Measured Lines')
-    plt.xlabel('Processing time of first 1000 records 1000 times')
-    plt.ylabel('Y axis')
-    plt.savefig('output.3.3.png')
-    print(time1000)
+    rez = change()
+    histogram(rez)
 
 if __name__ == "__main__":
     main()
-
