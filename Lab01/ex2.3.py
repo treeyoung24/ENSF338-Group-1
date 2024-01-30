050732a608bec6bd126a3f54c69dfcd19f09e308
Trevor Nguyen <trevorhuan@gmail.com>
2024-01-30 14:08:58 -0700

    created lab1 and lab2 folders

---
+++ Lab01/ex2.3.py
@@ -0,0 +1,19 @@
+import json
+
+f = open('large-file.json', 'r', encoding='utf-8')
+data = json.load(f)
+
+def change_size(data):
+    for record in data:
+        if 'size' in record:
+            record['size'] = 42
+    return 0
+
+def main():
+    change_size(data)
+    data.reverse()
+    with open('output.2.3.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+if __name__ == "__main__":
+    main()
