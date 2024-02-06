9d019e50122ae3ce36db7df0f1adfdc631d896ee
Zachary Lam <zachary.lam@ucalgary.ca>
2024-02-06 15:50:45 -0700

    partial ex1.py

--- Lab03/ex1.py
+++ Lab03/ex1.py
@@ -8,18 +8,26 @@ def merge_sort(arr, low, high):
         merge(arr, low, mid, high)
 
 def merge(arr, low, mid, high):
-    left = arr[low:mid+1]
-    right = arr[mid+1:high+1]
-    left.append(sys.maxsize)
-    right.append(sys.maxsize)
+    left = arr[low:mid + 1]
+    right = arr[mid + 1:high + 1]
     i = j = 0
-    for k in range(low, high+1):
-        if left[i] <= right[j]:
+    k = low
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
             arr[k] = left[i]
             i += 1
         else:
             arr[k] = right[j]
             j += 1
+        k += 1
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
 
 # The first while loop compares the elements from the left and right subarrays and is run for n times for each element.
 # The second and third while loops make sure the merge implementation continues even if one of the subarrays
