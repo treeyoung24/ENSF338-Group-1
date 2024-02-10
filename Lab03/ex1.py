9c735bbb74602a244dd4049d171753d85c3ab500
Zachary Lam <zachary.lam@ucalgary.ca>
2024-02-10 15:59:35 -0700

    finished ex1

--- Lab03/ex1.py
+++ Lab03/ex1.py
@@ -1,4 +1,6 @@
-import sys sys.setrecursionlimit(20000)
+import sys
+
+sys.setrecursionlimit(20000)
 
 def merge_sort(arr, low, high):
     if low < high:
@@ -28,12 +30,6 @@ def merge(arr, low, mid, high):
         arr[k] = right[j]
         j += 1
         k += 1
-
-# The first while loop compares the elements from the left and right subarrays and is run for n times for each element.
-# The second and third while loops make sure the merge implementation continues even if one of the subarrays
-# is empty. The second while loop is run for n/2 times and the third while loop is run for n/2 times.
-# The time complexity of the merge function is O(n) and since the time complexity of the dividing step is O(log n),
-# the overall algorithm has a worst-case complexity of O(n log n).
             
 arr = [8, 42, 25, 3, 3, 2, 27, 3]
 merge_sort(arr, 0, len(arr) - 1)
