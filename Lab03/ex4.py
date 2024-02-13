3d45e7e861a9b423b7a5e7b1cb3237401788b453
lordtrev4 <trevorhuan@gmail.com>
2024-02-12 23:12:54 -0700

    finish ex4

--- Lab03/ex4.py
+++ Lab03/ex4.py
@@ -18,7 +18,7 @@ def quick_sort(arr, low, high, num):
         return arr
     if low < high:
         pi = partition(arr, low, high)
-        print(f"for iteration {num}, array is {arr}") # print after every step
+        # print(f"for iteration {num}, array is {arr}") # print after every step
         num += 1
         quick_sort(arr, low, pi-1, num)
         quick_sort(arr, pi+1, high, num)
@@ -29,23 +29,23 @@ quick_sort(vector, 0, len(vector)-1, 1)
 
 print("quicksort for vector is complete. \n")
 
-# sizes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 300, 350, 400, 500, 600, 750, 850, 975]
-# quicktimes = []
+sizes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 300, 350, 400, 500, 600, 750, 850, 975]
+quicktimes = []
 
-# i = 0
-# for size in sizes:
-#     reversedlist = list(range(size, 0, -1)) # worst case where list is reversed
-#     start_time = time.time()
-#     quick_sort(reversedlist.copy(), 0, len(reversedlist)-1, 1)
-#     quicktimes.append(time.time() - start_time)
-#     print(f"Quick sort time for worst case for size {size} is {quicktimes[i]:.5f}")
-#     i += 1
+i = 0
+for size in sizes:
+    reversedlist = list(range(size, 0, -1)) # worst case where list is reversed
+    start_time = time.time()
+    quick_sort(reversedlist.copy(), 0, len(reversedlist)-1, 1)
+    quicktimes.append(time.time() - start_time)
+    print(f"Quick sort time for worst case for size {size} is {quicktimes[i]:.5f}")
+    i += 1
 
-# plt.plot(sizes, quicktimes)
-# plt.title('Quick Sort Time')
-# plt.xlabel('Input Size')
-# plt.ylabel('Time in seconds')
-# plt.show()
-# plt.close()
+plt.plot(sizes, quicktimes)
+plt.title('Quick Sort Time')
+plt.xlabel('Input Size')
+plt.ylabel('Time in seconds')
+plt.show()
+plt.close()
 
 
