42cbb592e7df7d7359101ae3331a916ff6f99897
Zachary Lam <zachary.lam@ucalgary.ca>
2024-02-25 23:00:41 -0700

    removed comments

--- Lab04/ex1.py
+++ Lab04/ex1.py
@@ -35,29 +35,23 @@ def binarySearch(head, value):
     last = None
 
     while True:
-        # Find middle
         mid = middle(start, last)
 
-        # If middle is empty
         if mid is None:
             return None
 
-        # If value is present at middle
         if mid.data == value:
             return mid
 
-        # If value is more than mid
         elif mid.data < value:
             start = mid.next
 
-        # If the value is less than mid.
         else:
             last = mid
 
         if not (last is None or last != start):
             break
 
-    # value not present
     return None
 
 class Array:
@@ -93,12 +87,10 @@ def main():
     for size in sizes:
         data = random.sample(range(1, size * 10), size)
 
-        # Create and populate array
         array = Array()
         for item in data:
             array.insert(item)
 
-        # Create and populate linked list
         head = None
         for item in sorted(data):
             if head is None:
@@ -111,11 +103,9 @@ def main():
 
         search_element = random.choice(data)
 
-        # Measure binary search time for array
         array_time = timeit.timeit(lambda: array.binary_search(search_element), number=1000)
         array_times.append(array_time)
 
-        # Measure binary search time for linked list
         linked_list_time = timeit.timeit(lambda: binarySearch(head, search_element), number=1000)
         linked_list_times.append(linked_list_time)
 
